8fd6d4fd62f5395490185f1c8ba0874b4f5e01ea
SairamCharanX <sairam.v99@gmail.com>
2023-01-18 23:57:58 +0530

    Arraylist

---
+++ CollectionsPractice.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class CollectionsPractice{
+    public static void main(String[] args){
+        ArrayList<Integer> intList = new ArrayList<>();
+        for(int x=0;x<10;x++){
+            intList.add(x);
+        }
+        System.out.println(intList.toString());
+    }
+}

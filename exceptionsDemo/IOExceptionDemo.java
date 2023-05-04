9ea306daeb9335ff28a1c960bf897e906ba42fa8
sairamcharanx <sairam.v99@gmail.com>
2023-05-05 03:09:49 +0530

    Exceptions_init

---
+++ exceptionsDemo/IOExceptionDemo.java
@@ -0,0 +1,16 @@
+package exceptionsDemo;
+
+public class IOExceptionDemo {
+    public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
+        try {
+            int[] integerArray = new int[10];
+            integerArray[11] = 1;
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Caught ArrayOutOfBoundsException");
+        }
+        finally {
+            System.out.println("Make sure the indices of the array are checked");
+        }
+    }
+}

833de823c5aa8a0a472ab3980633bc5d68915c6a
sairamcharanx <sairam.v99@gmail.com>
2023-05-08 03:18:28 +0530

    java_gui_init

---
+++ gui/swingDemo.java
@@ -0,0 +1,11 @@
+package gui;
+import javax.swing.*;
+
+class swingApplication{
+    public static void main(String[] args) {
+        JFrame jframe = new JFrame("mainWindow");
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setSize(400,400);
+        jframe.setVisible(true);
+    }
+}

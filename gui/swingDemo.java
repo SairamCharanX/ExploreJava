57441f6d832ee31355d5179d37481420d3bd8fe7
sairamcharanx <sairam.v99@gmail.com>
2023-05-08 03:21:48 +0530

    java_gui

--- gui/swingDemo.java
+++ gui/swingDemo.java
@@ -7,5 +7,13 @@ class swingApplication{
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setSize(400,400);
         jframe.setVisible(true);
+
+        JButton buttonOne = new JButton("click", null);
+        buttonOne.setBounds(130, 100, 10, 10);
+
+        jframe.add(buttonOne);
+
+
     }
+
 }

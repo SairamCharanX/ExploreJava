f55127c6f39493638d3cf4d8f4d26cbb84846cf0
sairamcharanx <sairam.v99@gmail.com>
2023-05-10 18:56:32 +0530

    cleanup

---
+++ servlets/firstServlet.java
@@ -0,0 +1,16 @@
+//package servlets;
+//
+//import java.io.*
+//import javax.servlet.*;
+//
+//
+//
+//
+//public class firstServlet implements Servlet{
+//    ServletConfig servletconfig = null;
+//
+//    publlc void init (ServletConfig servletconfig){
+//        this.config=config;
+//        System.out.println("Servlet initialized.");
+//    }
+//}

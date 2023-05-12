c08a1336344527d2666a1a4cfde24a35a9356186
sairamcharanx <sairam.v99@gmail.com>
2023-05-12 23:58:49 +0530

    Exc

---
+++ multiThreading/threadExceptionDemo.java
@@ -0,0 +1,12 @@
+package multiThreading;
+
+public class threadExceptionDemo extends Thread{
+
+    public void run(){
+        System.out.println("Thread running.");
+    }
+    public static void main(String[] args) throws IllegalStateException{
+        
+
+    }
+}

b788b1329b2b7afa71b8dc326283662bc045b604
sairamcharanx <sairam.v99@gmail.com>
2023-05-29 23:14:15 +0530

    Date and time

---
+++ dateAndTime/DateAndTimeDemo.java
@@ -0,0 +1,9 @@
+package dateAndTime;
+import java.util.*;
+
+public class DateAndTimeDemo{
+    public static void main(String[] args) {
+        java.util.Date date = new java.util.Date();
+        System.out.println(date.getTime());
+    }
+}

1b5d5f77254a6c1a22cde7a83642e59c75531716
sairamcharanx <sairam.v99@gmail.com>
2023-08-11 22:30:00 +0530

    HashMap

---
+++ collections/Maps.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Maps{
+
+    HashMap<Integer, String> map = new HashMap<>();
+    
+   
+}
--- data/src/main/java/Json.java
+++ data/src/main/java/Json.java
@@ -1,4 +1,4 @@
-public class DataConvert{
+public class Json {
     public static void main(String[] args){
         
     }

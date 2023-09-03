59317d23aa6ef1c1d8da07e47e881a84ee66e367
sairamcharanx <sairam.v99@gmail.com>
2023-09-03 23:48:59 +0530

    forEach

--- collections/Runner.java
+++ collections/Runner.java
@@ -1,3 +1,6 @@
+package collections;
+
+import java.util.*;
 import java.util.Scanner;
 
 public class Runner{
@@ -5,7 +8,7 @@ public class Runner{
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
-        List<Integer> numbers = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<Integer>();
 
         //Insert numbers into the list
         for(int index=0;index<10;index++){
@@ -13,8 +16,15 @@ public class Runner{
             numbers.add(temp);
         }
 
+        System.out.println("Regular Loop:");
         for(int number : numbers){
             System.out.println(number);
         }
+
+        //Using forEach
+        System.out.println("Using forEach:");
+        numbers.forEach(number -> {
+            System.out.println(number);
+        });
     }
 }

4c56bba3ddb5c151852a3e8646600fcbc03c9101
sairamcharanx <sairam.v99@gmail.com>
2023-09-04 22:10:14 +0530

    Streams

---
+++ collections/StreamRunner.java
@@ -0,0 +1,19 @@
+package collections;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.Arrays;
+
+public class StreamRunner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+        List<Integer> squares = numbers.stream().map(number -> number * number).collect(Collectors.toList());
+
+        System.out.println(numbers);
+        System.out.println(squares);
+    }
+}
